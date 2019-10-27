c9bd63a164cb1b960b16fd644030d89c86b8348e
Filip <fjsdlkfj>
2019-10-27 16:59:26 +0100

    Initial commit

---
+++ configtool3/resource.h
@@ -0,0 +1,41 @@
+// ####ECOSHOSTGPLCOPYRIGHTBEGIN####                                        
+// -------------------------------------------                              
+// This file is part of the eCos host tools.                                
+// Copyright (C) 1998, 1999, 2000, 2001, 2002 Free Software Foundation, Inc.
+//
+// This program is free software; you can redistribute it and/or modify     
+// it under the terms of the GNU General Public License as published by     
+// the Free Software Foundation; either version 2 or (at your option) any   
+// later version.                                                           
+//
+// This program is distributed in the hope that it will be useful, but      
+// WITHOUT ANY WARRANTY; without even the implied warranty of               
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        
+// General Public License for more details.                                 
+//
+// You should have received a copy of the GNU General Public License        
+// along with this program; if not, write to the                            
+// Free Software Foundation, Inc., 51 Franklin Street,                      
+// Fifth Floor, Boston, MA  02110-1301, USA.                                
+// -------------------------------------------                              
+// ####ECOSHOSTGPLCOPYRIGHTEND####                                          
+#include "CTCommonres.h"
+#ifdef PLUGIN
+    #include "eCosres.h"
+#else
+    #include "CTres.h"
+#endif
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
---
+++ configtool3/resource1.h
@@ -0,0 +1,86 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDB_BITMAP1                     101
+#define IDB_BITMAP2                     102
+#define IDB_BITMAP3                     103
+#define IDB_BITMAP4                     104
+#define IDB_BITMAP5                     105
+#define IDB_BITMAP6                     106
+#define IDB_BITMAP7                     107
+#define IDB_BITMAP8                     108
+#define IDB_BITMAP9                     109
+#define IDB_BITMAP10                    110
+#define IDB_BITMAP11                    111
+#define IDB_BITMAP12                    112
+#define IDB_BITMAP13                    113
+#define IDB_BITMAP14                    114
+#define IDB_BITMAP15                    115
+#define IDB_BITMAP16                    116
+#define IDI_ICON1                       117
+#define IDI_ICON2                       118
+#define IDI_ICON3                       119
+#define IDI_ICON4                       120
+#define IDI_ICON5                       121
+#define IDI_ICON6                       122
+#define IDI_ICON7                       123
+#define IDI_ICON8                       124
+#define IDI_ICON9                       125
+#define IDB_BITMAP17                    126
+#define IDB_BITMAP18                    127
+#define IDB_BITMAP19                    128
+#define IDB_BITMAP20                    129
+#define IDB_BITMAP21                    130
+#define IDB_BITMAP22                    131
+#define IDB_BITMAP23                    132
+#define IDB_BITMAP24                    133
+#define IDB_BITMAP25                    134
+#define IDB_BITMAP26                    135
+#define IDB_BITMAP27                    136
+#define IDB_BITMAP28                    137
+#define IDB_BITMAP29                    138
+#define IDB_BITMAP30                    139
+#define IDB_BITMAP31                    140
+#define IDB_BITMAP32                    141
+#define IDB_BITMAP33                    142
+#define IDB_BITMAP34                    143
+#define IDB_BITMAP35                    144
+#define IDB_BITMAP36                    145
+#define IDB_BITMAP37                    146
+#define IDB_BITMAP38                    147
+#define IDB_BITMAP39                    148
+#define IDB_BITMAP40                    149
+#define IDB_BITMAP41                    150
+#define IDB_BITMAP42                    151
+#define IDB_BITMAP43                    152
+#define IDB_BITMAP44                    153
+#define IDB_BITMAP45                    154
+#define IDB_BITMAP46                    155
+#define IDB_BITMAP47                    156
+#define IDB_BITMAP48                    157
+#define IDB_BITMAP49                    158
+#define IDB_BITMAP50                    159
+#define IDB_BITMAP51                    160
+#define IDB_BITMAP52                    161
+#define IDB_BITMAP53                    162
+#define IDB_BITMAP54                    163
+#define IDB_PNG1                        164
+#define IDB_BITMAP55                    165
+#define IDB_BITMAP56                    166
+#define IDB_BITMAP57                    167
+#define IDB_BITMAP58                    168
+#define IDB_BITMAP59                    169
+#define IDB_BITMAP60                    170
+#define IDB_BITMAP61                    171
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        172
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
